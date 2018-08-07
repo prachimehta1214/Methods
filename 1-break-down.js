d675d6bb525446a2c9508cfc9d56d99046038829
prachimehta1214 <42134655+prachimehta1214@users.noreply.github.com>
2018-08-07 10:59:18 +0200

    Update 1-break-down.js

--- 1-break-down.js
+++ 1-break-down.js
@@ -1,1 +1,33 @@
+let input = a string;
+let expected = see;
+let output;
 
+console.log("string(arg).charAt(2) + string("ee");\n);
+console.log("- " + input + " should become " + expected);
+
+{ //  (string, a string) -> (string, see)
+	const step_1 = String(input); 
+	// str: "1a string"
+	const step_2 = step_1.charAt(2, s); 
+	// str: "s"
+	const step_3 = + string("ee"); 
+	// string: "see"
+	output = step_3;
+};
+
+
+console.log("- actual: " + output );
+
+
+// empty trace-block 
+//	for studying different input/output pairs
+
+// { //  (___, ___) -> (#, ___)
+// 	const step_1 = String(input); 
+// 	// str: "a string"
+// 	const step_2 = step_1.charAt(2, "s"); 
+// 	// str: "s"
+// 	const step_3 = + String("ee");
+// 	// str: "see"
+// 	output = step_3;
+// };
